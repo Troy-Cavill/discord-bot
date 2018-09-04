d4c2fc61f7e8ea2ce5d1ffa20827c982f9aa27ff
Troy-Cavill <40477606+Troy-Cavill@users.noreply.github.com>
2018-09-05 10:00:04 +1200

    Update bot.py

--- bot.py
+++ bot.py
@@ -184,5 +184,4 @@ async def on_ready():
 
     await client.change_presence(game=discord.Game(name="d!help", type=0))
 
-bot_token = os.environ("BOT_TOKEN")
-client.run(bot_token)
+client.run(process.env.BOT_TOKEN)
